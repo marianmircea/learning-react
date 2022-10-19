0478cf1cc7d8dc4b2ffb1b7fd20e01a1bcc5d419
mariancd <marian@innertrends.com>
2022-10-19 12:07:18 +0300

    Params Lesson

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Car from './Car';
 
-
 function Garage () {
     const cars = [
         {id: 1, brand: 'Ford'},
         {id: 2, brand: 'BMW'},
         {id: 3, brand: 'Audi'}
     ];
+
     return (
         <>
-            <h1>Who lives in my garage?</h1>
+            <h1>Who liveeees in my garage?</h1>
             <ul>
-                {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+                {//cars.map((car) => <Car key={car.id} brand={car.brand} />)
+                }
             </ul>
         </>
     );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Garage />);
+//ReactDOM.createRoot(document.getElementById('root')).render(<Garage />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car brand='ceva' />);
