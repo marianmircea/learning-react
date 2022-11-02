085963766d520a62b71c036f58d1d628892b403c
mariancd <marian@innertrends.com>
2022-11-02 14:50:37 +0200

    Lesson - React Hooks

--- src/index.js
+++ src/index.js
@@ -83,5 +83,35 @@ function Timer2 () {
     return <h1>Rendered {count} times!</h1>
 }
 
+function ExampleWithCountButton() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+        //document.title = 'You clicked ' + count + ' times'; // working
+    });
+
+    var hello_message = function() {
+        return "Hello Everybody!";
+    }
+    var hello_message_new = () => {
+        return "Tot Hello Everybody!";
+    }
+    var hello_msg = () => 'Hello Evrbdy!';
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+            <p>
+                {hello_msg()}
+            </p>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(<ExampleWithCountButton />);
