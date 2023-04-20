7b95d8463b0dafd9828f8d67d6b0f33d9db5004f
mariancd <marian@innertrends.com>
2023-04-20 11:00:41 +0300

    Plugin understanding

--- Disable Admin Bar/disable_admin_bar.php
+++ Disable Admin Bar/disable_admin_bar.php
@@ -1,4 +1,8 @@
 <?php
     /**
-     * 
+     * Plugin Name: Disable Admin Bar
+     * Description: Learning how a WP plugin works
+     * Version: 1.0
+     * Author: Marian M.
+     * Author URI: https://www.linkedin.com/in/marian-mircea/
      */
