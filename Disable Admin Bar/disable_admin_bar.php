1cf16f548a4936a05766bb839ae79154f623bcdb
mariancd <marian@innertrends.com>
2023-04-20 14:22:53 +0300

    Plugin understanding

--- Disable Admin Bar/disable_admin_bar.php
+++ Disable Admin Bar/disable_admin_bar.php
@@ -1,8 +1,11 @@
 <?php
     /**
      * Plugin Name: Disable Admin Bar
-     * Description: Learning how a WP plugin works
+     * Description: Learning how a WP plugin works. Just disable the admin bar
      * Version: 1.0
      * Author: Marian M.
      * Author URI: https://www.linkedin.com/in/marian-mircea/
      */
+
+    // Remove the admin bar from the front end
+    add_filter( 'show_admin_bar', '__return_false' );
